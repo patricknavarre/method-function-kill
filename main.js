8bdb5229e1568538241a1b3002d70d085c39ee93
patricknavarre <patrick.navarre@codeimmersives.com>
2020-12-11 10:35:41 -0500

    to-be-continued

--- main.js
+++ main.js
@@ -2,6 +2,47 @@
  * YOUR CODE HERE *
  ******************/
 
+const Person = function (newFirstName, newLastName, newAge = 0, newMarried = false) {
+  return {
+    firstName : newFirstName,
+    lastName : newLastName, 
+    age : newAge, 
+    married : newMarried,
+
+    getFirstName: function() {
+      return `${this.firstName}`
+    },
+
+    getLastName: function() {
+      return `${this.lastName}`
+    },
+
+    goingOn: function() {
+      return this.age + 1
+    },
+
+    ageUp: function() {
+      return this.age++
+    },
+
+    getFullName: function() {
+      return `${this.firstName}` `${this.lastName}`
+    },
+
+
+
+
+
+
+    
+
+
+
+
+
+  }
+  
+}
 
 
 /*********************************
